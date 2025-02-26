c263400cda74e3a0e15fd874d65a045592495da8
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-02-26 20:33:33 -0300

    first commit

---
+++ src/test/java/com/zup/zupbank/ZupbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.zupbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
